bb8513b40bfc031a55d07d5784d7694bf7a07657
code-plet <70930547+code-plet@users.noreply.github.com>
2022-01-05 17:10:25 +0700

    New Repo

--- HRM_VTHP/NghiepVu/frmChamCong.cs
+++ HRM_VTHP/NghiepVu/frmChamCong.cs
@@ -249,12 +249,12 @@ namespace HRM_VTHP.NghiepVu
             // Fill a cell
             if (e.CellValue != null && e.CellValue.ToString().Trim() != "" && Convert.ToInt32(e.CellValue) == 2)
             {
-                Image image = Image.FromFile("c:\\lock.png");
+                Image image = Image.FromFile("D:\\important files\\PTPMOOP\\PTKTHDT\\HRM_VTHP\\Image\\lock.png");
                 e.Graphics.DrawImage(image, new Point(e.Bounds.Location.X + 35, e.Bounds.Location.Y));
             }
             if (e.CellValue != null && e.CellValue.ToString().Trim() != "" && Convert.ToInt32(e.CellValue) == 1)
             {
-                Image image = Image.FromFile("c:\\unlock.png");
+                Image image = Image.FromFile("D:\\important files\\PTPMOOP\\PTKTHDT\\HRM_VTHP\\Image\\unlock.png");
                 e.Graphics.DrawImage(image, new Point(e.Bounds.Location.X + 35, e.Bounds.Location.Y));
             }
         }
